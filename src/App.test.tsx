1df58270b009098529877d29ce29897f08f6c2be
jsm94 <smjuan24@gmail.com>
2023-08-30 11:24:25 +0200

    test: add test for podcast data table and update jest library to fix act warning

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,49 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+
 import App from "./App";
 
+import { mockEpisodesListData } from "./modules/podcasts/infra/mocks/mockEpisodesListData";
+import { mockPodcastsListData } from "./modules/podcasts/infra/mocks/mockPodcastsListData";
+
 describe("App", () => {
-  it("renders App component", () => {
-    render(<App />);
-    expect(screen.getByPlaceholderText(/podcast/i)).toBeInTheDocument();
+  beforeEach(() => {
+    console.log("beforeAll");
+    global.fetch = jest.fn((url) => {
+      console.log(url);
+      if (
+        url ===
+        "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json"
+      ) {
+        return Promise.resolve({
+          json: () => Promise.resolve(mockPodcastsListData),
+        });
+      }
+      return Promise.resolve({
+        json: () => Promise.resolve(mockEpisodesListData),
+      });
+    }) as jest.Mock<Promise<Response>>;
   });
 
-  it("renders a table", () => {
+  it("renders App component", async () => {
     render(<App />);
-    expect(screen.getByRole("table")).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByPlaceholderText(/podcast/i)).toBeInTheDocument();
+    });
+  });
+
+  it("renders a table", async () => {
+    render(<App />);
+    await waitFor(() => {
+      expect(screen.getByRole("table")).toBeInTheDocument();
+    });
+  });
+
+  it('renders a table with podcasts data "A History of Rock Music in 500 Songs"', async () => {
+    render(<App />);
+    await waitFor(() => {
+      expect(
+        screen.getByText(/A History of Rock Music in 500 Songs/i)
+      ).toBeInTheDocument();
+    });
   });
 });
