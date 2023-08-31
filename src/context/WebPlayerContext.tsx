93c6b212d8e8e69e6f50cd739e261b3ab78ad5ff
jsm94 <smjuan24@gmail.com>
2023-08-31 13:34:00 +0200

    feat: add context for update/traking web player state

---
+++ src/context/WebPlayerContext.tsx
@@ -0,0 +1,111 @@
+import { createContext, useContext, useReducer } from "react";
+import { Episode } from "../modules/podcasts/domain/Episode";
+
+export enum WebPlayerActionTypes {
+  SET_TRACKS = "set_tracks",
+  PLAY = "play",
+  PAUSE = "pause",
+  NEXT = "next",
+  PREV = "prev",
+  LOOP = "loop",
+  SHUFFLE = "shuffle",
+  SEEK = "seek",
+  SET_VOLUME = "set_volume",
+}
+
+export type WebPlayerState = {
+  tracks: Episode[];
+  currentTrackIndex: number;
+  isPlaying: boolean;
+  isLooping: boolean;
+  isShuffling: boolean;
+  volume: number;
+  currentTime: number;
+};
+
+export type WebPlayerAction = WebPlayerState & {
+  type: WebPlayerActionTypes;
+};
+
+const initialState: WebPlayerState = {
+  tracks: [],
+  currentTrackIndex: 0,
+  isPlaying: false,
+  isLooping: false,
+  isShuffling: false,
+  volume: 0.25,
+  currentTime: 0,
+};
+
+const WebPlayerReducer = (state: WebPlayerState, action: WebPlayerAction) => {
+  switch (action.type) {
+    case WebPlayerActionTypes.SET_TRACKS: {
+      return { ...state, tracks: action.tracks };
+    }
+    case WebPlayerActionTypes.PLAY: {
+      return { ...state, isPlaying: true };
+    }
+    case WebPlayerActionTypes.PAUSE: {
+      return { ...state, isPlaying: false };
+    }
+    case WebPlayerActionTypes.NEXT: {
+      const nextTrackIndex =
+        state.currentTrackIndex === state.tracks.length - 1
+          ? 0
+          : state.currentTrackIndex + 1;
+      return { ...state, currentTrackIndex: nextTrackIndex };
+    }
+    case WebPlayerActionTypes.PREV: {
+      const prevTrackIndex =
+        state.currentTrackIndex === 0
+          ? state.tracks.length - 1
+          : state.currentTrackIndex - 1;
+      return { ...state, currentTrackIndex: prevTrackIndex };
+    }
+    case WebPlayerActionTypes.LOOP: {
+      return { ...state, isLooping: !state.isLooping };
+    }
+    case WebPlayerActionTypes.SHUFFLE: {
+      return { ...state, isShuffling: !state.isShuffling };
+    }
+    case WebPlayerActionTypes.SEEK: {
+      return { ...state, currentTime: action.currentTime };
+    }
+    case WebPlayerActionTypes.SET_VOLUME: {
+      return { ...state, volume: action.volume };
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
+};
+
+const WebPlayerContext = createContext(initialState);
+
+const WebPlayerDispatchContext = createContext(
+  (() => {}) as React.Dispatch<WebPlayerAction>
+);
+
+type WebPlayerProviderProps = {
+  children: React.ReactNode;
+};
+
+export const WebPlayerProvider = ({ children }: WebPlayerProviderProps) => {
+  const [state, dispatch] = useReducer(WebPlayerReducer, initialState);
+
+  return (
+    <WebPlayerContext.Provider value={state}>
+      <WebPlayerDispatchContext.Provider value={dispatch}>
+        {children}
+      </WebPlayerDispatchContext.Provider>
+    </WebPlayerContext.Provider>
+  );
+};
+
+export const useWebPlayer = () => {
+  return useContext(WebPlayerContext);
+};
+
+export const useWebPlayerDispatch = () => {
+  return useContext(WebPlayerDispatchContext);
+};
