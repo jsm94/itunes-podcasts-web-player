3ebfebe516b97f08967e70c016adfb2f1722b546
jsm94 <smjuan24@gmail.com>
2023-09-02 20:34:08 +0200

    feat: add back to home button on header

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,13 @@
+import { Link, useLocation } from "react-router-dom";
+
 import { useSearchDispatch } from "../context/SearchContext";
 
 import { cn } from "../utils/helpers";
 
-import { Icon, Icons } from "../components/Icon";
+import { Icon, IconSizes, Icons } from "../components/Icon";
 import WebPlayer from "../components/WebPlayer/WebPlayer";
 import Input from "../components/ui/Input";
+import { ROUTES } from "../constants/app.constants";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -12,6 +15,7 @@ type LayoutProps = {
 };
 
 const Layout = ({ children, className }: LayoutProps) => {
+  const { pathname } = useLocation();
   const dispatch = useSearchDispatch();
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -20,7 +24,14 @@ const Layout = ({ children, className }: LayoutProps) => {
 
   return (
     <div className={cn(["flex flex-col gap-[34px] pt-[30px]", className])}>
-      <header className="container mx-auto">
+      <header className="container flex gap-4 mx-auto">
+        {pathname !== ROUTES.HOME && (
+          <Link to={ROUTES.HOME}>
+            <div className="flex p-[15px] items-center align-center bg-eerie-black rounded-[15px]">
+              <Icon icon={Icons.BACK} size={IconSizes.MEDIUM} />
+            </div>
+          </Link>
+        )}
         <Input
           className="w-full"
           startAdornment={
